cfc15c5e1d73bc0a8d3095c727cde648aec55560
Melody T <124547562+staletoast@users.noreply.github.com>
2025-01-27 20:07:58 +1100

    Update password.py

--- password.py
+++ password.py
@@ -2,10 +2,10 @@
 def check_password(password):
     if len(password) < 9:
         return False
-        print ("password is under 9 characters")
-    if len(password) > 12:
+        print ("password must be minimum 9 characters")
+    if len(password) > 16:
         return False
-        print ("password is above 12 characters")
+        print ("password must be maximum 16 characters")
     if not name.isalpha():
         return False
         print ("password does not contain letters")
